b82f59737e9e23e9e534eafb9082c23bb38bfd34
Jeeyoung J SHin <littlejt73@gmail.com>
2022-03-22 21:43:45 -0700

    commit2

--- beginning/js/script.js
+++ beginning/js/script.js
@@ -5,17 +5,16 @@ let name = document.getElementById('name');
 let ext = document.getElementById('ext');
 let email = document.getElementById('email');
 let department = document.getElementById('department');
-//2. 폼을 불러서 이벤트 서밋이 괄호안의 펑션을 행한다.
+//2. 폼을 불러서 이벤트 서밋이 괄호안의 function을 행한다.
 window.addEventListener('load', init, false)
-   function init(){
-       "use strict";
-   form.addEventListener("submit", () => {
-        console.log('id: '+  id.value);
-        console.log('name: ' + name.value);
-        console.log('ext: '+ ext.value);
-        console.log('email: ' + email.value);
-        console.log('department: ' + department.value);
-        event.preventDefault();  
-});
-}
+   function init(){"use strict";
+        form.addEventListener("submit", () => {
+                console.log('id: '+  id.value);
+                console.log('name: ' + name.value);
+                console.log('ext: '+ ext.value);
+                console.log('email: ' + email.value);
+                console.log('department: ' + department.value);
+                event.preventDefault();  
+                }); 
+            }
 
